05b6fa940cec0ee00a375fd18acaee7eeb38fade
scpviki <arun.letmein@gmail.com>
2026-03-23 14:47:39 +0400

    hello

---
+++ SQUARE.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen().bgcolor("white")
+square=turtle.Turtle()
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+turtle.done
